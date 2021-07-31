87fb89ded9ae22ef976bb212bfe3b417384724ed
mlkorra <korrarahuldev@gmail.com>
2021-07-31 11:24:15 +0530

    Update App

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import hhi
+def main():
+
+    app = MultiApp()
+    #app.add_app("Competitive Analysis",topic_model.app)
+    #app.add_app("Trading View Dashboard",trading_view_db.app)
+    app.add_app("HHI ",hhi.app)
+    #app.add_app("Growth",growth.app)
+    #app.add_app("About",about.app)
+    app.run()
+
+if __name__ == "__main__":
+    main()
