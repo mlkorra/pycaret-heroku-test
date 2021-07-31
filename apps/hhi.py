46521f4eac6df6cd6607d342de36b32d0f4c27ce
mlkorra <korrarahuldev@gmail.com>
2021-07-31 13:12:46 +0530

    Update App

--- apps/hhi.py
+++ apps/hhi.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import os
-import pycaret
 from pathlib import Path
-from pycaret.regression import *
 from joblib import load
+from sklearn.ensemble import AdaBoostRegressor
+import numpy as np
 
 def app():
     
@@ -25,25 +25,34 @@ def app():
     data = load_data()
     st.write(data)
 
+    #@st.cache(allow_output_mutation=True)
+    def load_model(path='apps/hhi_sklearn1.pkl'):
+        model = load(path)
+        return model
 
-    clf = load('apps/hhi.joblib')
+    clf = load_model()
+
     if st.sidebar.checkbox("Predit HHI", False):
         st.header("Predit HHI of a startup based on these features")
         # Declare a form and call methods directly on the returned object
         f = st.form(key='ml_form')
-        competitor_count = [0,0,0,0,0,0,0,0,0,0]
-        competitor_count[f.selectbox("Competitor count (cap at 10)", [1, 2, 3,4,5,6,7,8,9,10]) -1] = 1
+        #competitor_count = [0,0,0,0,0,0,0,0,0,0]
+        #competitor_count[f.selectbox("Competitor count (cap at 10)", [1, 2, 3,4,5,6,7,8,9,10]) -1] = 1
+        competitor_count = f.selectbox("Competitor count (cap at 10)",np.arange(1,11)) 
         company_revenue = f.number_input(label='Total Company Revenue in Millions')
         competitor_revenue = f.number_input(label='Total Competitor Revenue in Millions')
         submitted = f.form_submit_button(label='Submit!')
+        #clf = load_model()
         if submitted:
-            my_data = np.array([company_revenue,competitor_count[0],competitor_revenue,competitor_count[9],competitor_count[5],competitor_count[3],competitor_count[8],competitor_count[7],competitor_count[6],competitor_count[1],competitor_count[2],competitor_count[4]])
+            #my_data = np.array([company_revenue,competitor_count[0],competitor_revenue,competitor_count[9],competitor_count[5],competitor_count[3],competitor_count[8],competitor_count[7],competitor_count[6],competitor_count[1],competitor_count[2],competitor_count[4]])
+            my_data = np.array([competitor_count,company_revenue,competitor_revenue])
             print(my_data)
             try:
                 prediction = clf.predict([my_data])
+                st.write(prediction[0])
             except Exception as e:
                 f.error(f"Could not make prediction. {e}")
-            st.write(prediction[0])
+            
 
-        pass
+    
 
